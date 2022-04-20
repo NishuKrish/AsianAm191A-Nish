7bda0a346e62bbd964747e5338b62d6f2b69ac86
NishuKrish <52436176+NishuKrish@users.noreply.github.com>
2022-04-20 13:06:45 -0700

    week3 updates

--- week3/js/new.js
+++ week3/js/new.js
@@ -1,11 +1,11 @@
 // declare the map
-const map = L.map('a_map').setView([34.0709,-100.444], 8);
+const map = L.map('a_map').setView([34.0709,-100.444], 3);
 
 
 
-L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+}).addTo(map); 
 
 
 fetch("map.geojson")
