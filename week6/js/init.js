dcfdbfef1a521f8420e4d0ee1ea283204db40967
NishuKrish <52436176+NishuKrish@users.noreply.github.com>
2022-05-11 13:01:02 -0700

    added week6

--- week6/js/init.js
+++ week6/js/init.js
@@ -1,5 +1,5 @@
 // declare variables
-let mapOptions = {'center': [34.0709,-100.444],'zoom':4}
+let mapOptions = {'center': [34.0709,-96.6989],'zoom':6}
 
 // use the variables
 const map = L.map('the_map').setView(mapOptions.center, mapOptions.zoom);
@@ -9,10 +9,25 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 // create a function to add markers
-function addMarker(lat,lng,title,message, loc){
-    console.log(message)
-    L.marker([lat,lng]).addTo(map).bindPopup(`<h2>${title}</h2> <h3>${message}</h3> <h3>${loc}</h3> `)
-    return message
+function addMarker(data){
+    // console.log(data)
+    // these are the names of our lat/long fields in the google sheets:
+    L.circle([data.lat,data.lng], 5000).addTo(map).bindPopup(`<h2>${data['Name']}</h2> <h3>${data['Like']}</h3> <h3>${data['Location']}</h3>`)
+    createButtons(data.lat,data.lng,data['Name'])
+    return
+}
+// create a function to add the buttons
+function createButtons(lat,lng,title){
+    const newButton = document.createElement("button"); // adds a new button
+    newButton.id = "button"+title; // gives the button a unique id
+    newButton.innerHTML = title; // gives the button a title
+    newButton.setAttribute("lat",lat); // sets the latitude 
+    newButton.setAttribute("lng",lng); // sets the longitude 
+    newButton.addEventListener('click', function(){
+        map.flyTo([lat,lng]); //this is the flyTo from Leaflet
+    })
+    const spaceForButtons = document.getElementById('placeForButtons')
+    spaceForButtons.appendChild(newButton);//this adds the button to our page.
 }
 
 const dataUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQNveTEgSo8JDBmiso8aW97wE7EKiPkERUJEbLymC5h8lfZ3L7j4rF-rT0rzENvU-rQLQZyGCUxmJoY/pub?output=csv"
@@ -29,8 +44,7 @@ function processData(results){
     console.log(results)
     results.data.forEach(data => {
         console.log(data)
-        addMarker(data.lat,data.lng,data['Name'],data['Like'], data['Location'])
+        addMarker(data)
     })
 }
-
 loadData(dataUrl)
